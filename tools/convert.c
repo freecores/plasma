286f06179d629cdc4c995adfb970135242b33d6c
rhoads <rhoads@8fa3edc1-71da-4d9b-a61a-8b20347ad903>
2002-01-16 03:10:30 +0000

    Properly hander missing sections

--- tools/convert.c
+++ tools/convert.c
@@ -4,27 +4,24 @@
 #include <stdlib.h>
 
 #define BUF_SIZE (1024*1024)
+/*Assumes running on PC little endian*/
 #define ntohl(A) ((A>>24)|((A&0x00ff0000)>>8)|((A&0xff00)<<8)|(A<<24))
 
-enum {
-   TEXT_OFFSET,  TEXT_LENGTH,
-   RDATA_OFFSET, RDATA_LENGTH,
-   DATA_OFFSET,  DATA_LENGTH,
-   SDATA_OFFSET, SDATA_LENGTH,
-   SBSS_OFFSET,  SBSS_LENGTH,
-   BSS_OFFSET,   BSS_LENGTH
-};
+#define CODE_START 0x60
+#define SECTION_START 0x4c
+#define SECTION_END 0x160
+#define SECTION_SIZE 0x28
+#define SECTION_OFFSET 0xc
+#define SECTION_LENGTH 0x10
 
-long code_start_offset=0x60;
-unsigned long map[12];
-long offset[]={
-   0x58, 0x5c,  //.text offset,length
-   0x80, 0x84,  //.rdata offset,length
-   0xa8, 0xac,  //.data offset,length
-   0xd0, 0xd4,  //.sdata offset,length
-   0xf8, 0xfc,  //.sbss offset,length
-   0x120,0x124  //.bss offset,length
-};
+struct header_t {
+   unsigned long text_offset,text_length;
+   unsigned long rdata_offset,rdata_length;
+   unsigned long data_offset,data_length;
+   unsigned long sdata_offset,sdata_length;
+   unsigned long sbss_offset,sbss_length;
+   unsigned long bss_offset,bss_length;
+} header;
 
 unsigned long load(char *ptr,unsigned long address)
 {
@@ -48,8 +45,8 @@ int main(int argc,char *argv[])
 {
    FILE *infile,*outfile,*txtfile;
    unsigned char *buf,*code;
-   long size,code_offset;
-   unsigned long i,d,gp_ptr;
+   long size;
+   unsigned long code_offset,index,name,offset,length,d,i,gp_ptr;
 
    printf("test.exe -> code.txt & test2.exe\n");
    infile=fopen("test.exe","rb");
@@ -61,32 +58,87 @@ int main(int argc,char *argv[])
    size=fread(buf,1,BUF_SIZE,infile);
    fclose(infile);
 
-   code_offset=load(buf,code_start_offset);
-   printf("code_offset=0x%x\n",code_offset);
+   code_offset=load(buf,CODE_START);
+   printf("code_offset=0x%x ",code_offset);
    code=buf+code_offset;
+
    /*load all of the segment offsets and lengths*/
-   for(i=0;i<12;++i) {
-      map[i]=load(buf,offset[i]);
-   }
-   if(code_offset<0x120) {
-      printf("no .sdata\n");
-      for(i=6;i<12;i+=2) {
-         map[i]=map[4];
-         map[i+1]=map[5];
+   for(index=SECTION_START;index<code_offset-0x20;index+=SECTION_SIZE) {
+      name=load(buf,index);
+      offset=load(buf,index+SECTION_OFFSET);
+      length=load(buf,index+SECTION_LENGTH);
+      switch(name) {
+      case 0x2e746578: /*.text*/
+         header.text_offset=offset;
+         header.text_length=length;
+         offset+=length;
+         length=0;
+         header.rdata_offset=offset;
+         header.rdata_length=length;
+         header.data_offset=offset;
+         header.data_length=length;
+         header.sdata_offset=offset;
+         header.sdata_length=length;
+         header.sbss_offset=offset;
+         header.sbss_length=length;
+         header.bss_offset=offset;
+         header.bss_length=length;
+         break;
+      case 0x2e726461: /*.rdata*/
+         header.rdata_offset=offset;
+         header.rdata_length=length;
+         offset+=length;
+         length=0;
+         header.data_offset=offset;
+         header.data_length=length;
+         header.sdata_offset=offset;
+         header.sdata_length=length;
+         header.sbss_offset=offset;
+         header.sbss_length=length;
+         header.bss_offset=offset;
+         header.bss_length=length;
+         break;
+      case 0x2e646174: /*.data*/
+         header.data_offset=offset;
+         header.data_length=length;
+         offset+=length;
+         length=0;
+         header.sdata_offset=offset;
+         header.sdata_length=length;
+         header.sbss_offset=offset;
+         header.sbss_length=length;
+         header.bss_offset=offset;
+         header.bss_length=length;
+         break;
+      case 0x2e736461: /*.sdata*/
+         header.sdata_offset=offset;
+         header.sdata_length=length;
+         offset+=length;
+         length=0;
+         header.sbss_offset=offset;
+         header.sbss_length=length;
+         header.bss_offset=offset;
+         header.bss_length=length;
+         break;
+      case 0x2e736273: /*.sbss*/
+         header.sbss_offset=offset;
+         header.sbss_length=length;
+         offset+=length;
+         length=0;
+         header.bss_offset=offset;
+         header.bss_length=length;
+         break;
+      case 0x2e627373:  /*.bss*/
+         header.bss_offset=offset;
+         header.bss_length=length;
+         break;
+      default: printf("unknown 0x%x\n",name);
       }
-   } else if(code_offset<0x140) {
-      printf("no .rdata\n");
-      for(i=11;i>4;--i) {
-         map[i]=map[i-2];
-      }
-   }
-   for(i=0;i<12;i+=2) {
-      printf("0x%x 0x%x\n",map[i],map[i+1]);
    }
 
    /*Initialize the $gp register for sdata and sbss*/   
-   gp_ptr=map[SDATA_OFFSET]+0x8000;
-   printf("gp_ptr=0x%x\n",gp_ptr);
+   gp_ptr=header.sdata_offset+0x8000;
+   printf("gp_ptr=0x%x ",gp_ptr);
    /*modify the first opcodes in boot.asm*/
    /*modify the lui opcode*/
    set_low(code,0,gp_ptr>>16);
@@ -95,14 +147,14 @@ int main(int argc,char *argv[])
 
    /*Clear .sbss and .bss*/
    printf(".sbss=0x%x .bss_end=0x%x\n",
-      map[SBSS_OFFSET],map[BSS_OFFSET]+map[BSS_LENGTH]);
-   set_low(code,8,map[SBSS_OFFSET]);
-   set_low(code,12,map[BSS_OFFSET]+map[BSS_LENGTH]);
+      header.sbss_offset,header.bss_offset+header.bss_length);
+   set_low(code,8,header.sbss_offset);
+   set_low(code,12,header.bss_offset+header.bss_length);
 
    /*write out code.txt*/
    outfile=fopen("test2.exe","wb");
    txtfile=fopen("code.txt","w");
-   for(i=0;i<=map[SDATA_OFFSET]+map[SDATA_LENGTH];i+=4) {
+   for(i=0;i<=header.sdata_offset+header.sdata_length;i+=4) {
       d=load(code,i);
       fprintf(txtfile,"%8.8x\n",d);
       fwrite(code+i,4,1,outfile);
