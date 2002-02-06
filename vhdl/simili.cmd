6c0eb80236fc98cf6557753dac02a4da465f52c4
rhoads <rhoads@8fa3edc1-71da-4d9b-a61a-8b20347ad903>
2002-02-06 21:14:22 +0000

    Added comments

--- vhdl/simili.cmd
+++ vhdl/simili.cmd
@@ -1,4 +1,7 @@
 add list /u1/pc /u1/opcode /u1/*
 #add list /tbench/*
-SetListStyle nodelta collapse
+#add list /u1/u4_reg_bank/bank1/*
+#add list /u1/u3_control/*
+#add list /u1/u8_mult/*
+#SetListStyle nodelta collapse
 SetListInterval 0us 1ms
