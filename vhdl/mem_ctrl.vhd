c5004b57192f04a6b0436d1c4a2c83179b190ed6
rhoads <rhoads@8fa3edc1-71da-4d9b-a61a-8b20347ad903>
2002-06-25 03:39:09 +0000

    accurate_timing, cleanup, pipeline

--- vhdl/mem_ctrl.vhd
+++ vhdl/mem_ctrl.vhd
@@ -21,6 +21,7 @@ use ieee.std_logic_1164.all;
 use work.mlite_pack.all;
 
 entity mem_ctrl is
+   generic(ACCURATE_TIMING : boolean := false);
    port(clk          : in std_logic;
         reset_in     : in std_logic;
         pause_in     : in std_logic;
@@ -38,8 +39,7 @@ entity mem_ctrl is
         mem_data_w   : out std_logic_vector(31 downto 0);
         mem_data_r   : in std_logic_vector(31 downto 0);
         mem_byte_sel : out std_logic_vector(3 downto 0);
-        mem_write    : out std_logic;
-        mem_pause    : in std_logic);
+        mem_write    : out std_logic);
 end; --entity mem_ctrl
 
 architecture logic of mem_ctrl is
@@ -47,7 +47,7 @@ architecture logic of mem_ctrl is
    constant little_endian  : std_logic_vector(1 downto 0) := "00";
    signal opcode_reg       : std_logic_vector(31 downto 0);
    signal next_opcode_reg  : std_logic_vector(31 downto 0);
-   signal mem_byte_sel_reg : std_logic_vector(3 downto 0);
+   signal address_reg      : std_logic_vector(31 downto 0);
 
    subtype mem_state_type is std_logic_vector(1 downto 0);
    signal mem_state_reg  : mem_state_type;
@@ -55,71 +55,81 @@ architecture logic of mem_ctrl is
    constant STATE_ADDR   : mem_state_type := "01";
    constant STATE_WRITE  : mem_state_type := "10";
    constant STATE_PAUSE  : mem_state_type := "11";
+
+   --ACCURATE_TIMING notes:
+   --The VHDL compiler's timing calculation isn't able to realize that
+   --memory reads take two clock cycles.  It notices that reg_bank:reg_dest
+   --is dependent on mem_ctrl:mem_data_r which is dependent on 
+   --mem_ctrl:mem_address which is dependent on alu:c_alu.  However,
+   --this dependency is only true for memory read or write cycles
+   --which are multiple clock cycles.  Enabling ACCURATE_TIMING
+   --creates an additional 32-bit register that does nothing other
+   --than letting the VHDL compiler accurately predict the maximum
+   --clock speed.
 begin
 
 mem_proc: process(clk, reset_in, pause_in, nullify_op, 
                   address_pc, address_data, mem_source, data_write, 
-                  mem_data_r, mem_pause,
+                  mem_data_r,
                   opcode_reg, next_opcode_reg, mem_state_reg,
-                  mem_byte_sel_reg)
-   variable data, datab   : std_logic_vector(31 downto 0);
-   variable opcode_next   : std_logic_vector(31 downto 0);
-   variable byte_sel_next : std_logic_vector(3 downto 0);
-   variable write_line    : std_logic;
+                  address_reg)
+   variable data           : std_logic_vector(31 downto 0);
+   variable opcode_next    : std_logic_vector(31 downto 0);
+   variable byte_sel       : std_logic_vector(3 downto 0);
+   variable write_line     : std_logic;
    variable mem_state_next : mem_state_type;
-   variable pause         : std_logic;
-   variable address_next  : std_logic_vector(31 downto 0);
-   variable bits          : std_logic_vector(1 downto 0);
-   variable mem_data_w_v  : std_logic_vector(31 downto 0);
+   variable pause          : std_logic;
+   variable address        : std_logic_vector(31 downto 0);
+   variable bits           : std_logic_vector(1 downto 0);
+   variable mem_data_w_v   : std_logic_vector(31 downto 0);
 begin
-   byte_sel_next := "0000";
+   byte_sel := "0000";
    write_line := '0';
    pause := '0';
    mem_state_next := mem_state_reg;
 
-   address_next := address_pc;
-   data := mem_data_r;
-   datab := ZERO;
+   address := address_pc;
+   data := ZERO;
    mem_data_w_v := ZERO; 
 
    case mem_source is
    when mem_read32 =>
-      datab := data;
+      data := mem_data_r;
    when mem_read16 | mem_read16s =>
-      if address_data(1) = little_endian(1) then
-         datab(15 downto 0) := data(31 downto 16);
+      if address_reg(1) = little_endian(1) then
+         data(15 downto 0) := mem_data_r(31 downto 16);
       else
-         datab(15 downto 0) := data(15 downto 0);
+         data(15 downto 0) := mem_data_r(15 downto 0);
       end if;
-      if mem_source = mem_read16 or datab(15) = '0' then
-         datab(31 downto 16) := ZERO(31 downto 16);
+      if mem_source = mem_read16 or data(15) = '0' then
+         data(31 downto 16) := ZERO(31 downto 16);
       else
-         datab(31 downto 16) := ONES(31 downto 16);
+         data(31 downto 16) := ONES(31 downto 16);
       end if;
    when mem_read8 | mem_read8s =>
-      bits := address_data(1 downto 0) xor little_endian;
+      bits := address_reg(1 downto 0) xor little_endian;
       case bits is
-      when "00" => datab(7 downto 0) := data(31 downto 24);
-      when "01" => datab(7 downto 0) := data(23 downto 16);
-      when "10" => datab(7 downto 0) := data(15 downto 8);
-      when others => datab(7 downto 0) := data(7 downto 0);
+      when "00" => data(7 downto 0) := mem_data_r(31 downto 24);
+      when "01" => data(7 downto 0) := mem_data_r(23 downto 16);
+      when "10" => data(7 downto 0) := mem_data_r(15 downto 8);
+      when others => data(7 downto 0) := mem_data_r(7 downto 0);
       end case;
-      if mem_source = mem_read8 or datab(7) = '0' then
-         datab(31 downto 8) := ZERO(31 downto 8);
+      if mem_source = mem_read8 or data(7) = '0' then
+         data(31 downto 8) := ZERO(31 downto 8);
       else
-         datab(31 downto 8) := ONES(31 downto 8);
+         data(31 downto 8) := ONES(31 downto 8);
       end if;
    when mem_write32 =>
       write_line := '1';
       mem_data_w_v := data_write;
-      byte_sel_next := "1111";
+      byte_sel := "1111";
    when mem_write16 =>
       write_line := '1';
       mem_data_w_v := data_write(15 downto 0) & data_write(15 downto 0);
       if address_data(1) = little_endian(1) then
-         byte_sel_next := "1100";
+         byte_sel := "1100";
       else
-         byte_sel_next := "0011";
+         byte_sel := "0011";
       end if;
    when mem_write8 =>
       write_line := '1';
@@ -128,13 +138,13 @@ begin
       bits := address_data(1 downto 0) xor little_endian;
       case bits is
       when "00" =>
-         byte_sel_next := "1000"; 
+         byte_sel := "1000"; 
       when "01" => 
-         byte_sel_next := "0100"; 
+         byte_sel := "0100"; 
       when "10" =>
-         byte_sel_next := "0010"; 
+         byte_sel := "0010"; 
       when others =>
-         byte_sel_next := "0001"; 
+         byte_sel := "0001"; 
       end case;
    when others =>
    end case;
@@ -142,45 +152,44 @@ begin
    opcode_next := opcode_reg;
    if mem_source = mem_fetch then --opcode fetch
       mem_state_next := STATE_FETCH; 
-      if pause_in = '0' and mem_pause = '0' then
-         opcode_next := data;
+      if pause_in = '0' then
+         opcode_next := mem_data_r;
       end if;
    else  --data read or write (not opcode fetch)
+
+      --State Machine
       case mem_state_reg is
       when STATE_FETCH =>
          write_line := '0';
          pause := '1';
-         if address_data(31) = '0' then   --4 cycle write
-            byte_sel_next := "0000";
-         end if;
-         if mem_pause = '0' then
+         byte_sel := "0000";
+         if pause_in = '0' then
             mem_state_next := STATE_ADDR;
          end if;
       when STATE_ADDR =>  --address lines pre-hold
-         address_next := address_data;
-         if write_line = '1' and address_data(31) = '0' then
+         address := address_reg;
+         if write_line = '1' and address_reg(31) = '1' then
             pause := '1';
-            if mem_pause = '0' then
+            byte_sel := "0000";
+            if pause_in = '0' then
                mem_state_next := STATE_WRITE;    --4 cycle access
             end if;
          else
-            byte_sel_next := "0000";
-            if mem_pause = '0' then
+            if pause_in = '0' then
                opcode_next := next_opcode_reg;
                mem_state_next := STATE_FETCH;    --2 cycle access
             end if;
          end if;
       when STATE_WRITE =>
          pause := '1';
-         address_next := address_data;
-         byte_sel_next := "0000";
-         if mem_pause = '0' then
+         address := address_reg;
+         if pause_in = '0' then
             mem_state_next := STATE_PAUSE; 
          end if;
       when OTHERS =>  --STATE_PAUSE address lines post-hold
-         address_next := address_data;
-         byte_sel_next := "0000";
-         if mem_pause = '0' then
+         address := address_reg;
+         byte_sel := "0000";
+         if pause_in = '0' then
             opcode_next := next_opcode_reg;
             mem_state_next := STATE_FETCH;
          end if;
@@ -194,23 +203,30 @@ begin
    if reset_in = '1' then
       mem_state_reg <= STATE_FETCH;
       opcode_reg <= ZERO;
-	  next_opcode_reg <= ZERO;
+      next_opcode_reg <= ZERO;
       write_line := '0';
-      mem_byte_sel_reg <= "0000";
    elsif rising_edge(clk) then
       mem_state_reg <= mem_state_next;
       opcode_reg <= opcode_next;
       if mem_state_reg = STATE_FETCH then
-         next_opcode_reg <= data;
+         next_opcode_reg <= mem_data_r;
       end if;
-      mem_byte_sel_reg <= byte_sel_next;
+   end if;
+
+   if rising_edge(clk) then
+      if ACCURATE_TIMING then
+         address_reg <= address_data;
+      end if;
+   end if;
+   if not ACCURATE_TIMING then
+      address_reg <= address_data;
    end if;
 
    opcode_out <= opcode_reg;
-   data_read <= datab;
-   pause_out <= mem_pause or pause;
-   mem_byte_sel <= mem_byte_sel_reg;
-   mem_address <= address_next;
+   data_read <= data;
+   pause_out <= pause;
+   mem_byte_sel <= byte_sel;
+   mem_address <= address;
    mem_write <= write_line;
 
    if write_line = '1' then
