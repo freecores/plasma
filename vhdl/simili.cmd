cd748a25cccd39ed8f522f169153d4fdcc7008f9
(no author) <(no author)@8fa3edc1-71da-4d9b-a61a-8b20347ad903>
2004-06-08 02:46:24 +0000

    This commit was manufactured by cvs2svn to create tag 'V2_1'.

--- vhdl/simili.cmd
+++
@@ -1,8 +0,0 @@
-add list /u1/*
-#add list /u1/u1_cpu/*
-#add list /u1/u1_cpu/u8_mult/*
-#add list /u1/u1_cpu/pc /u1/u1_cpu/opcode /u1/*
-#add list /u1/u1_cpu/u3_control/*
-#add list /u1/u1_cpu/u8_mult/*
-SetListStyle nodelta collapse
-SetListInterval 0us 1ms
