f5a3c877b51e59b830bc462788315fef497ab68b
rhoads <rhoads@8fa3edc1-71da-4d9b-a61a-8b20347ad903>
2006-09-10 01:00:51 +0000

    Added UartPrintfNull()

--- kernel/uart.c
+++ kernel/uart.c
@@ -395,6 +395,11 @@ void UartPrintfCritical(const char *format,
 }
 
 
+void UartPrintfNull(void)
+{
+}
+
+
 void UartScanf(const char *format,
                int arg0, int arg1, int arg2, int arg3,
                int arg4, int arg5, int arg6, int arg7)
